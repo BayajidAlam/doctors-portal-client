cdbe273d3128f69a489481867332c3d071b96d0b
bayajidalam <bayajidalam2001@gmail.com>
2023-02-02 02:35:14 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import footerImg from '../../../assets/images/footer.png';
 
 const Footer = () => {
   return (
+    <footer
+    style={{
+      background: `url(${footerImg})`,
+      backgroundSize: 'contain'
+    }}
+    className='my-20'>
+    <div className="footer p-10">
     <div>
-          <h2>Footer</h2>
+      <span className="footer-title">Services</span> 
+      <Link to='/'className="link link-hover">Branding</Link>
+      <Link to='/'className="link link-hover">Design</Link>
+      <Link to='/'className="link link-hover">Marketing</Link>
+      <Link to='/'className="link link-hover">Advertisement</Link>
+    </div> 
+    <div>
+      <span className="footer-title">Company</span> 
+      <Link to='/'className="link link-hover">About us</Link>
+      <Link to='/'className="link link-hover">Contact</Link>
+      <Link to='/'className="link link-hover">Jobs</Link>
+      <Link to='/'className="link link-hover">Press kit</Link>
+    </div> 
+    <div>
+      <span className="footer-title">Legal</span> 
+      <Link to='/'className="link link-hover">Terms of use</Link>
+      <Link to='/'className="link link-hover">Privacy policy</Link>
+      <Link to='/'className="link link-hover">Cookie policy</Link>
     </div>
+    </div>
+    <div>
+    <p className='text-center mb-4'>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+  </div>
+  </footer>
+  
   );
 };
 
