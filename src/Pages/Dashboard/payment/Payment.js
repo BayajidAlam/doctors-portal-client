cff1e93865617696987b7749fc4b9f4f71929099
bayajidalam <bayajidalam2001@gmail.com>
2023-02-08 01:39:08 +0600

    ok for this day

--- src/Pages/Dashboard/payment/Payment.js
+++ src/Pages/Dashboard/payment/Payment.js
@@ -1,17 +1,37 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import CheckOut from "./CheckOut";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Payment = () => {
-  const {treatment, price, appointmentDate} = useLoaderData();
-  
+  const booking =  useLoaderData()
+  const { treatment, price, appointmentDate } = booking;
+
   return (
     <div>
       <div className="flex justify-center items-center">
-        <div className="card w-1/2 h-[600px] bg-blue-100 shadow-2xl">
+        <div className="card w-1/2 h-[300px] bg-blue-100 shadow-2xl">
           <div className="card-body">
-            <h2 className="text-2xl text-blue-400 font-bold font text-center">Payment</h2>
-            <h1 className="text-xl">Payment for <span className="font-semibold">{treatment}</span></h1>
-            <p className="text-xl">Your slot -<span className="font-semibold">{appointmentDate}</span></p>
+            <h2 className="text-2xl text-blue-400 font-bold font text-center">
+              Payment
+            </h2>
+            <h1 className="text-xl">
+              Payment for <span className="font-semibold">{treatment}</span>
+            </h1>
+            <p className="text-xl">
+              Your slot -
+              <span className="font-semibold">{appointmentDate}</span>
+            </p>
+            <div className="w-2/3 bg-base-100  h-32 p-1 shadow-xl rounded-lg mx-auto">
+              <Elements stripe={stripePromise}>
+                <CheckOut
+                  booking={booking}
+                />
+              </Elements>
+            </div>
           </div>
         </div>
       </div>
